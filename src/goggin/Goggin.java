94070c966cbb1402df69ff3138ae1a02f216cdc5
jgoggin206@gmail.com <>
2020-02-04 16:13:01 -0500

    Jarrett Goggin

---
+++ src/goggin/Goggin.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package goggin;
+
+/**
+ *
+ * @author jgogg
+ */
+public class Goggin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Jarrett();
+    }
+    public static void Jarrett() {
+       
+        System.out.println("Jarrett Goggin");
+    }
+}
